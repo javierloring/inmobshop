d800cdb2f0a429dc1e2d3514871d0c10ac52c24a
javierloring <jloringm@gmail.com>
2020-05-29 01:52:07 +0200

    modificación de config.php quitado raiz inmobshop\

--- config.php
+++ config.php
@@ -1,27 +1,27 @@
 <?php
 #---------------------------DEFINICION DE CONSTANTES
 # Favicon
-define("FAVICON", "\inmobshop\\favicon.png");
+define("FAVICON", "\\favicon.png");
 # navegacion general
-define("HOME_INMOBSHOP", "\inmobshop\index.php");
-define("LOGO_INMOBSHOP", "\inmobshop\media\logo\inmobshop_2_orange.png");
-// define("BUSCAR_OFERTAS", "\inmobshop\presentacion\buscar-ofertas.php");
-define("BUSCAR_OFERTAS", "\inmobshop\presentacion\\form-buscar-mostrar-ofertas.php");
-define("CREA_TU_ANUNCIO", "\inmobshop\presentacion\crear-anuncio.php");
-define("REGISTRATE", "\inmobshop\presentacion\\registro.php");
-define("INICIA_SESION", "\inmobshop\presentacion\iniciar-sesion.php");
-define("CIERRA_SESION", "\inmobshop\\negocio\cerrar-sesion.php");
+define("HOME_INMOBSHOP", "\index.php");
+define("LOGO_INMOBSHOP", "\media\logo_2_orange.png");
+// define("BUSCAR_OFERTAS", "\presentacion\buscar-ofertas.php");
+define("BUSCAR_OFERTAS", "\presentacion\\form-buscar-mostrar-ofertas.php");
+define("CREA_TU_ANUNCIO", "\presentacion\crear-anuncio.php");
+define("REGISTRATE", "\presentacion\\registro.php");
+define("INICIA_SESION", "\presentacion\iniciar-sesion.php");
+define("CIERRA_SESION", "\\negocio\cerrar-sesion.php");
 
 # navegación administrador
 
 #navegación gestor
 
 #navegación Partucular-Profesional-Demandante
-define("CONTRATOS_PARTICULARES", "\inmobshop\presentacion\ag-particular-contratos.php");
-define("ANUNCIOS_PARTICULARES", "\inmobshop\presentacion\ag-particular-anuncios.php");
-define("CONTACTOS_PARTICULARES", "\inmobshop\presentacion\ag-particular-contactos.php");
-define("BUSQUEDAS_Y_FAVORITOS_PARTICULARES", "\inmobshop\presentacion\ag-particular-busquedas&favoritos.php");
-define("PERFIL_PARTICULARES", "\inmobshop\presentacion\ag-particular-perfil.php");
+define("CONTRATOS_PARTICULARES", "\presentacion\ag-particular-contratos.php");
+define("ANUNCIOS_PARTICULARES", "\presentacion\ag-particular-anuncios.php");
+define("CONTACTOS_PARTICULARES", "\presentacion\ag-particular-contactos.php");
+define("BUSQUEDAS_Y_FAVORITOS_PARTICULARES", "\presentacion\ag-particular-busquedas&favoritos.php");
+define("PERFIL_PARTICULARES", "\presentacion\ag-particular-perfil.php");
 #navegación Particular-Profesional
 
 #------------------------------------------------------------------cabecerasCORS
